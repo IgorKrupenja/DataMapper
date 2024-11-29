9f087ca67913f62f21def4ec9866af341d1fcbfd
Igor Krupenja <igor.krupenja@gmail.com>
2024-11-29 16:01:57 +0200

    Fix puppeteer issues

--- js/generate/pdfToBase64.js
+++ js/generate/pdfToBase64.js
@@ -2,16 +2,6 @@ import * as puppeteer from 'puppeteer';
 
 
 export async function generatePdfToBase64(template, res) {
-  // let file = { content: template };
-  // let options = { format: "A4" };
-
-  // try {
-  //   const pdfBuffer = await html_to_pdf.generatePdf(file, options);
-  //   res.json({ response: pdfBuffer.toString("base64") });
-  // } catch (err) {
-  //   res.sendStatus(500);
-  // }
-
   try {
     const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-dev-shm-usage'] });
 
@@ -19,14 +9,13 @@ export async function generatePdfToBase64(template, res) {
     await page.setContent(template, { waitUntil: "load", timeout: 0 });
     const buffer = await page.pdf({
       format: "A4",
-      landscape: true,
       printBackground: true,
       timeout: 0,
     });
 
     await browser.close();
     // todo move res logic to controller
-    res.json({ response: buffer.toString("base64") });
+    res.json({ response: Buffer.from(buffer).toString("base64") });
   } catch (err) {
     res.sendStatus(500);
   }
