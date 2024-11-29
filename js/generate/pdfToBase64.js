b11ce4830d1a5584c58769b6e1e11cc0a3cb14f9
Igor Krupenja <igor.krupenja@gmail.com>
2024-11-29 16:41:42 +0200

    Add logging

--- js/generate/pdfToBase64.js
+++ js/generate/pdfToBase64.js
@@ -1,19 +1,24 @@
 import * as puppeteer from 'puppeteer';
 
-
-export async function generatePdfToBase64(template, res) {
+export async function generatePdfToBase64(html, res) {
   try {
+    const t0 = performance.now();
+    console.log("convertHtmlToPdf: generating from HTML:\n\n", html);
+  
     const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-dev-shm-usage'] });
-
+  
     const page = await browser.newPage();
-    await page.setContent(template, { waitUntil: "load", timeout: 0 });
+    await page.setContent(html, { waitUntil: "load", timeout: 0 });
     const uintArray = await page.pdf({
       format: "A4",
       printBackground: true,
       timeout: 0,
     });
-
+  
     await browser.close();
+  
+    const t1 = performance.now();
+    console.log(`convertHtmlToPdf: generated in ${t1 - t0} milliseconds`);
     // todo move res logic to controller
     res.json({ response: Buffer.from(uintArray).toString("base64") });
   } catch (err) {
