b3f298ffdc38b63e2d97d6299d2de7361b6be711
Igor Krupenja <igor.krupenja@gmail.com>
2024-12-05 12:43:40 +0200

    Clean up

--- js/generate/convertHtmlToPdf.js
+++ js/generate/convertHtmlToPdf.js
@@ -1,9 +1,6 @@
 import * as puppeteer from 'puppeteer';
 
 export async function convertHtmlToPdf(html) {
-  const t0 = performance.now();
-  console.log("convertHtmlToPdf: generating from HTML:\n\n", html);
-  
   const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-dev-shm-usage'] });
   
   const page = await browser.newPage();
@@ -16,9 +13,6 @@ export async function convertHtmlToPdf(html) {
   const base64 = Buffer.from(uintArray).toString("base64");
   
   await browser.close();
-
-  const t1 = performance.now();
-  console.log(`convertHtmlToPdf: generated in ${t1 - t0} milliseconds`);
 
   return base64;
 }
