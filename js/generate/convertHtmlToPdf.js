31d08afc79905b3e8af4457b53a3b87a5852d45a
Igor Krupenja <igor.krupenja@gmail.com>
2025-01-22 11:12:55 +0200

    Add try catch

--- js/generate/convertHtmlToPdf.js
+++ js/generate/convertHtmlToPdf.js
@@ -4,25 +4,31 @@ export async function convertHtmlToPdf(html) {
   const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-dev-shm-usage', '--enable-logging', '--v=1'], dumpio: true, });
   console.debug("convertHtmlToPdf: created browser")
   
-  const page = await browser.newPage();
-  await page.setContent(html, { waitUntil: "load", timeout: 0 });
-  
-  console.debug("convertHtmlToPdf: created page")
+  try {
+    const page = await browser.newPage();
+    await page.setContent(html, { waitUntil: "load", timeout: 0 });
+    
+    console.debug("convertHtmlToPdf: created page")
 
-  const uintArray = await page.pdf({
-    format: "A4",
-    printBackground: true,
-    timeout: 0,
-  });
-  const base64 = Buffer.from(uintArray).toString("base64");
-  console.debug("convertHtmlToPdf: created pdf")
+    const uintArray = await page.pdf({
+      format: "A4",
+      printBackground: true,
+      timeout: 0,
+    });
+    const base64 = Buffer.from(uintArray).toString("base64");
+    console.debug("convertHtmlToPdf: created pdf")
 
-  // Closing the page explicitly should not be needed normally
-  // But this may be bugged in Puppeteer: https://github.com/puppeteer/puppeteer/issues/7922
-  await page.close();
-  console.debug("convertHtmlToPdf: closed page")
-  await browser.close();
-  console.debug("convertHtmlToPdf: closed browser")
+    // Closing the page explicitly should not be needed normally
+    // But this may be bugged in Puppeteer: https://github.com/puppeteer/puppeteer/issues/7922
+    await page.close();
+    console.debug("convertHtmlToPdf: closed page")
+    await browser.close();
+    console.debug("convertHtmlToPdf: closed browser")
 
-  return base64;
+    return base64;
+  } catch (error) {
+    console.error("convertHtmlToPdf: error", error);
+    await browser.close();
+    throw error;
+  }
 }
